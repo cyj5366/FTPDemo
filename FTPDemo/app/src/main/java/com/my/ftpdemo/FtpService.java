746511935759b7b8493962a5bb917417eeb130de
jack.chen <cyj5366@users.noreply.github.com>
2020-03-12 10:04:47 +0800

    Anonymous login
    修改成无需账户密码登陆ftp文件下载
    Anonymous login

--- FTPDemo/app/src/main/java/com/my/ftpdemo/FtpService.java
+++ FTPDemo/app/src/main/java/com/my/ftpdemo/FtpService.java
@@ -10,18 +10,32 @@ import android.text.TextUtils;
 import android.util.Log;
 import android.widget.Toast;
 
+import org.apache.ftpserver.ConnectionConfigFactory;
 import org.apache.ftpserver.FtpServer;
 import org.apache.ftpserver.FtpServerFactory;
+import org.apache.ftpserver.ftplet.Authentication;
+import org.apache.ftpserver.ftplet.AuthenticationFailedException;
 import org.apache.ftpserver.ftplet.Authority;
 import org.apache.ftpserver.ftplet.FtpException;
+import org.apache.ftpserver.ftplet.User;
+import org.apache.ftpserver.ftplet.UserManager;
 import org.apache.ftpserver.listener.ListenerFactory;
+import org.apache.ftpserver.usermanager.AnonymousAuthentication;
+import org.apache.ftpserver.usermanager.ClearTextPasswordEncryptor;
+import org.apache.ftpserver.usermanager.PasswordEncryptor;
+import org.apache.ftpserver.usermanager.PropertiesUserManagerFactory;
+import org.apache.ftpserver.usermanager.UserManagerFactory;
+import org.apache.ftpserver.usermanager.UsernamePasswordAuthentication;
+import org.apache.ftpserver.usermanager.impl.AbstractUserManager;
 import org.apache.ftpserver.usermanager.impl.BaseUser;
+import org.apache.ftpserver.usermanager.impl.ConcurrentLoginPermission;
 import org.apache.ftpserver.usermanager.impl.WritePermission;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -46,7 +60,7 @@ public class FtpService extends Service {
     @Override
     public void onCreate() {
         super.onCreate();
-        rootPath = Environment.getExternalStorageDirectory().getAbsolutePath();
+        rootPath = "/sdcard/";//Environment.getExternalStorageDirectory().getAbsolutePath();
 
         try {
             init();
@@ -79,18 +93,25 @@ public class FtpService extends Service {
         FtpServerFactory serverFactory = new FtpServerFactory();
 
         //设置访问用户名和密码还有共享路径
-        BaseUser baseUser = new BaseUser();
-        baseUser.setName(user);
-        baseUser.setPassword(password);
-        baseUser.setHomeDirectory(rootPath);
+//        BaseUser baseUser = new BaseUser();
+//        baseUser.setName("anonymou");
+//        //baseUser.setPassword(password);
+//        baseUser.setHomeDirectory(rootPath);
+//
+//        List<Authority> authorities = new ArrayList<Authority>();
+//        authorities.add(new WritePermission());
+//        baseUser.setAuthorities(authorities);
+//        serverFactory.getUserManager().save(baseUser);
 
-        List<Authority> authorities = new ArrayList<Authority>();
-        authorities.add(new WritePermission());
-        baseUser.setAuthorities(authorities);
-        serverFactory.getUserManager().save(baseUser);
-
+        //设置无需账户密码访问
+        ConnectionConfigFactory connectionConfigFactory = new ConnectionConfigFactory();
+        connectionConfigFactory.setAnonymousLoginEnabled(true);
+        serverFactory.setConnectionConfig(connectionConfigFactory.createConnectionConfig());
+        serverFactory.setUserManager(new TestUserManagerFactory().createUserManager());
 
         ListenerFactory factory = new ListenerFactory();
+        //PropertiesUserManagerFactory userManagerFactory = new PropertiesUserManagerFactory();
+        //serverFactory.setUserManager(userManagerFactory.createUserManager());
         factory.setPort(port); //设置端口号 非ROOT不可使用1024以下的端口
         serverFactory.addListener("default", factory.createListener());
 
@@ -111,6 +132,86 @@ public class FtpService extends Service {
         if (server != null) {
             server.stop();
             server = null;
+        }
+    }
+
+    private class TestUserManagerFactory implements UserManagerFactory {
+
+        @Override
+        public UserManager createUserManager() {
+            return new TestUserManager("admin", new ClearTextPasswordEncryptor());
+        }
+    }
+    public String TEST_USERNAME = user;
+    public String TEST_PASSWORD = password;
+    private class TestUserManager extends AbstractUserManager {
+        private BaseUser testUser;
+        private BaseUser anonUser;
+
+        public TestUserManager(String adminName, PasswordEncryptor passwordEncryptor) {
+            super(adminName, passwordEncryptor);
+
+            testUser = new BaseUser();
+            testUser.setAuthorities(Arrays.asList(new Authority[] {new ConcurrentLoginPermission(1, 1)}));
+            testUser.setEnabled(true);
+            testUser.setHomeDirectory(rootPath);
+            testUser.setMaxIdleTime(10000);
+            testUser.setName(TEST_USERNAME);
+            testUser.setPassword(TEST_PASSWORD);
+
+            anonUser = new BaseUser(testUser);
+            anonUser.setName("anonymous");
+        }
+
+        @Override
+        public User getUserByName(String username) throws FtpException {
+            if(TEST_USERNAME.equals(username)) {
+                return testUser;
+            } else if(anonUser.getName().equals(username)) {
+                return anonUser;
+            }
+
+            return null;
+        }
+
+        @Override
+        public String[] getAllUserNames() throws FtpException {
+            return new String[] {TEST_USERNAME, anonUser.getName()};
+        }
+
+        @Override
+        public void delete(String username) throws FtpException {
+            //no opt
+        }
+
+        @Override
+        public void save(User user) throws FtpException {
+            //no opt
+            System.out.println("save");
+        }
+
+        @Override
+        public boolean doesExist(String username) throws FtpException {
+            return (TEST_USERNAME.equals(username) || anonUser.getName().equals(username)) ? true : false;
+        }
+
+        @Override
+        public User authenticate(Authentication authentication) throws AuthenticationFailedException {
+            if(UsernamePasswordAuthentication.class.isAssignableFrom(authentication.getClass())) {
+                UsernamePasswordAuthentication upAuth = (UsernamePasswordAuthentication) authentication;
+
+                if(TEST_USERNAME.equals(upAuth.getUsername()) && TEST_PASSWORD.equals(upAuth.getPassword())) {
+                    return testUser;
+                }
+
+                if(anonUser.getName().equals(upAuth.getUsername())) {
+                    return anonUser;
+                }
+            } else if(AnonymousAuthentication.class.isAssignableFrom(authentication.getClass())) {
+                return anonUser;
+            }
+
+            return null;
         }
     }
 
--- FTPDemo/app/src/main/java/com/my/ftpdemo/MainActivity.java
+++ FTPDemo/app/src/main/java/com/my/ftpdemo/MainActivity.java
@@ -1,9 +1,13 @@
 package com.my.ftpdemo;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -18,6 +22,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         textView = findViewById(R.id.textView);
+        //申请SD卡存储权限
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission_group.STORAGE) !=
+                PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 0);
+        }
 
         String ip = getIp();
         if(TextUtils.isEmpty(ip)){
@@ -26,7 +36,8 @@ public class MainActivity extends AppCompatActivity {
             String str = "请在IE浏览器上输入网址访问FTP服务\n" +
                     "ftp://"+ip+":2221\n" +
                     "账号:admin\n" +
-                    "密码:123456";
+                    "密码:123456\n"+
+                    "根目录:/sdcard/";
             textView.setText(str);
         }
 
